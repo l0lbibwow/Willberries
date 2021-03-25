fda195a308da6d1ee31037f1a0d2dc52702e8830
Bibigul Otepbergenova <bibigul.tulegenovna@mail.ru>
2021-03-25 18:11:41 +0600

    change main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ const mySwiper = new Swiper('.swiper-container', {
 
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
-const modalClose = document.querySelector('.modal-close');
 const openModal = function () {
 	modalCart.classList.add('show')
 };
@@ -18,22 +17,88 @@ const closeModal = function () {
 	modalCart.classList.remove('show')
 };
 buttonCart.addEventListener('click', openModal);
-modalClose.addEventListener('click', closeModal)
 
+
+modalCart.addEventListener('click',event  => {
+	const target = event.target;
+	if (event.target.classList.contains('overlay') || target.classList.contains('modal-close')){
+		closeModal()
+	}
+})
 //scroll smooth
 
 const scrollLinks = document.querySelectorAll('a.scroll-link');
 
+for (const scrollLink of scrollLinks){
 
-for (let i = 0; i < scrollLinks.length; i++){
-	console.log('ddfgsdfg')
-	scrollLinks[i].addEventListener('click', function(event) {
-		console.log('ddfgsdfg')
+	scrollLink.addEventListener('click', function(event) {
+
 		event.preventDefault();
-		const id = scrollLinks[i].getAttribute('href');
+		const id = scrollLink.getAttribute('href');
 		document.querySelector(id).scrollIntoView({
 			behavior: 'smooth',
 			block: 'start'
 		})
 	});
 }
+//goods
+
+const more = document.querySelector('.more');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async function (){
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		throw 'Errorochka' + result.status;
+	}
+	return await  result.json();
+}
+const createCard = function (objCard) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';
+
+	card.innerHTML = `
+	<div class="goods-card">
+	${objCard.label ? `<span class="label">${objCard.label}</span>` : ``}
+\t\t\t\t\t\t
+\t\t\t\t\t\t<img src="db/${objCard.img}" alt=${objCard.name} class="goods-image">
+\t\t\t\t\t\t<h3 class="goods-title">${objCard.name}</h3>
+\t\t\t\t\t\t<p class="goods-description">${objCard.description}</p>
+\t\t\t\t\t\t<button class="button goods-card-btn add-to-cart" data-id=${objCard.id}>
+\t\t\t\t\t\t\t<span class="button-price">$ ${objCard.price}</span>
+\t\t\t\t\t\t</button>
+\t\t\t\t\t</div>
+	`;
+	return card
+};
+const renderCards = function (data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard)
+	longGoodsList.append(...cards)
+	document.body.classList.add('show-goods')
+};
+
+more.addEventListener('click', function (event){
+	event.preventDefault()
+	getGoods().then(renderCards)
+});
+
+const filterCards = function(field,value) {
+	getGoods()
+		.then(function (data){
+		const filteredGoods = data.filter((function (good){
+			return good[field] === value
+		}))
+		return filteredGoods;
+	})
+		.then(renderCards);
+}
+navigationLink.forEach(function(link) {
+	link.addEventListener('click', function (event){
+		event.preventDefault();
+		const field = link.dataset.field;
+		const value = link.textContent;
+		filterCards(field,value);
+	})
+});
